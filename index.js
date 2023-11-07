c34cc3b971d4e3bdd5e7d09cdba53bd24751b882
chillsan99 <chilliary17023@gmail.com>
2023-11-07 00:24:52 -0500

    Updated Cooldown.

--- index.js
+++ index.js
@@ -4,17 +4,18 @@ const path = require('node:path');
 const {Client, Collection, Events, GatewayIntentBits} = require('discord.js');
 const {token} = require('./config.json');
 
+
+
 //create new client instance
-
 const client = new Client ({ intents:[GatewayIntentBits.Guilds]});
+//------
 
+client.cooldowns = new Collection();
 client.commands = new Collection();
+
+//Search Folders --
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
-
-const commandsPath = path.join(__dirname, 'commands');
-
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for(const folder of commandFolders){
     const commandsPath = path.join(foldersPath, folder);
@@ -36,6 +37,8 @@ client.once(Events.ClientReady, () => {
     console.log('Ready!');
 })
 
+
+
 client.on(Events.InteractionCreate, async interaction => {
     if(!interaction.isChatInputCommand()) return; 
     
@@ -45,6 +48,29 @@ client.on(Events.InteractionCreate, async interaction => {
         console.error(`No command matching ${interaction.commandName} was found.`);
         return;
     }
+
+    const {cooldowns} = client;
+
+    if(!cooldowns.has(command.data.name)){
+        cooldowns.set(command.data.name, new Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.data.name);
+    const defaultCooldownDuration = 3;
+    const cooldownAmt = (command.cooldown ?? defaultCooldownDuration) * 1000;
+
+    if (timestamps.has(interaction.user.id)) {
+		const expirationTime = timestamps.get(interaction.user.id) + cooldownAmt;
+
+		if (now < expirationTime) {
+			const expiredTimestamp = Math.round(expirationTime / 1000);
+			return interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
+		}
+	}
+
+    timestamps.set(interaction.user.id, now);
+	setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmt);
 
     try{
         await command.execute(interaction);
@@ -57,5 +83,7 @@ client.on(Events.InteractionCreate, async interaction => {
             }
         } 
 });
+
+
 
 client.login(token);
